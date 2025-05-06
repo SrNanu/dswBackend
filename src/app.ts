34966c52360e3d7b6193581fa14c3193bd385053
JuaniFiele <juanfiele2002@gmail.com>
2025-05-06 19:49:35 -0300

    cambio puerto

--- src/app.ts
+++ src/app.ts
@@ -43,6 +43,6 @@ app.use((_, res) => {
 /*await syncSchema() //never in production, only for development, when in production, the schema should be permanent*/
 
 //activar el servidor
-app.listen(37434, "yamanote.proxy.rlwy.net", () => {
+app.listen(3000, "yamanote.proxy.rlwy.net", () => {
   //console.log("Server running on http://localhost:3000/")
 })
